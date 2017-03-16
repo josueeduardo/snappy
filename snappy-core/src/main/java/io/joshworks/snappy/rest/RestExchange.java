3009f0bcad69a512e1cf09b1fac2901c7ce03698
Josh Gontijo <josue.eduardo206@gmail.com>
2017-03-16 21:21:40 +0000

    Added additional http methods

--- snappy-core/src/main/java/io/joshworks/snappy/rest/RestExchange.java
+++ snappy-core/src/main/java/io/joshworks/snappy/rest/RestExchange.java
@@ -3,9 +3,11 @@ package io.joshworks.snappy.rest;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.HeaderMap;
 import io.undertow.util.HeaderValues;
+import io.undertow.util.Headers;
 import io.undertow.util.PathTemplateMatch;
 
 import java.util.Deque;
+import java.util.Map;
 
 /**
  * Created by josh on 3/5/17.
@@ -44,13 +46,57 @@ public class RestExchange {
         return pathMatch.getParameters().get(key);
     }
 
-    public Deque<String> queryParameters(String key) {
+    public Map<String, Deque<String>> queryParams() {
+        return httpServerExchange.getQueryParameters();
+    }
+
+    public String queryParams(String key) {
+        Deque<String> params = httpServerExchange.getQueryParameters().get(key);
+        return params.isEmpty() ? null : params.getFirst();
+    }
+
+    public Deque<String> queryParamsValues(String key) {
         return httpServerExchange.getQueryParameters().get(key);
     }
 
-    public String queryParameter(String key) {
-        Deque<String> parameters = httpServerExchange.getQueryParameters().get(key);
-        return parameters.getFirst();
+    public String protocol() {
+        return httpServerExchange.getProtocol().toString();
+    }
+
+    public String host() {
+        return httpServerExchange.getHostName();
+    }
+
+    public int port() {
+        return httpServerExchange.getHostPort();
+    }
+
+    public String method() {
+        return httpServerExchange.getRequestMethod().toString();
+    }
+
+    public String scheme() {
+        return httpServerExchange.getRequestScheme();
+    }
+
+    public String path() {
+        return httpServerExchange.getRequestPath();
+    }
+
+    public String userAgent() {
+        HeaderValues userAgent = httpServerExchange.getRequestHeaders().get(Headers.USER_AGENT);
+        if (userAgent != null && !userAgent.isEmpty()) {
+            return userAgent.getFirst();
+        }
+        return null;
+    }
+
+    public MediaType type() {
+        HeaderValues contentType = httpServerExchange.getRequestHeaders().get(Headers.CONTENT_TYPE);
+        if (contentType != null && !contentType.isEmpty()) {
+            return MediaType.valueOf(contentType.getFirst());
+        }
+        return MediaType.WILDCARD_TYPE;
     }
 
     public Response response() {
