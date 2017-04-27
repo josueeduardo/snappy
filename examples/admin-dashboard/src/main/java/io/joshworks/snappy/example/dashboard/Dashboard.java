9dc8722591dd5400a1971e1aab832dc88914f53d
Josh Gontijo <josue.eduardo206@gmail.com>
2017-04-27 19:26:41 +0100

    Added dashboard empty example

---
+++ examples/admin-dashboard/src/main/java/io/joshworks/snappy/example/dashboard/Dashboard.java
@@ -0,0 +1,17 @@
+package io.joshworks.snappy.example.dashboard;
+
+import io.joshworks.snappy.extensions.dashboard.DashboardExtension;
+
+import static io.joshworks.snappy.SnappyServer.register;
+import static io.joshworks.snappy.SnappyServer.start;
+
+/**
+ * Created by Josh Gontijo on 4/27/17.
+ */
+public class Dashboard {
+
+    public static void main(String[] args) {
+        register(new DashboardExtension());
+        start();
+    }
+}
