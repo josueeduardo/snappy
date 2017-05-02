3e045fe36d345a3d822825caf50e73c654bfa766
Josh Gontijo <josue.eduardo206@gmail.com>
2017-05-02 23:48:42 +0100

    Removed hardcoded path

--- examples/admin-dashboard/src/main/java/io/joshworks/snappy/example/dashboard/Dashboard.java
+++ examples/admin-dashboard/src/main/java/io/joshworks/snappy/example/dashboard/Dashboard.java
@@ -1,6 +1,11 @@
 package io.joshworks.snappy.example.dashboard;
 
 import io.joshworks.snappy.extensions.dashboard.DashboardExtension;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 import static io.joshworks.snappy.SnappyServer.*;
 
@@ -9,8 +14,31 @@ import static io.joshworks.snappy.SnappyServer.*;
  */
 public class Dashboard {
 
-    public static void main(String[] args) {
+    private static final String LOG_PATH = System.getProperty("user.home") + File.separator + "sample.log";
+
+    public static void main(String[] args) throws Exception {
+        copyDummyLog();
+        onShutdown(Dashboard::deleteDummyLog);
+
         register(new DashboardExtension());
-        start();
+        cors();
+        start(); //http://localhost:9100/
+
+    }
+
+    //Used to show the tailf functionality, edit me on ~/sample.log and see the live changes on admin console
+    private static void copyDummyLog() throws Exception {
+        //copies a dummy log file to home directory
+        File dest = new File(LOG_PATH);
+        dest.createNewFile();
+
+        try (InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("sample.log");
+             FileOutputStream output = new FileOutputStream(dest)) {
+            IOUtils.copy(input, output);
+        }
+    }
+
+    private static void deleteDummyLog() {
+        new File(LOG_PATH).delete();
     }
 }
