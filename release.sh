9265bff5a4acc7076448daa49b69f507e6bd1497
Josh <josue.eduardo206@gmail.com>
2019-08-01 01:35:54 +0100

    Release 0.4.1

--- release.sh
+++ release.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-FILE1=$1
 if [[ -z "$1" ]]
   then
     echo "No version supplied"
@@ -9,14 +8,14 @@ fi
 
 echo "Releasing version $1"
 
-mvn versions:set -DnewVersion=$1 -DgenerateBackupPoms=false
+mvn versions:set -DnewVersion="$1" -DgenerateBackupPoms=false
 mvn clean install
 
 git commit -a -m "Release $1"
 git push origin master
 
-git tag -a $1 -m "Release $1"
-git push origin $1
+git tag -a "$1" -m "Release $1"
+git push origin "$1"
 
 mvn clean deploy -P release
 mvn nexus-staging:release -P release
